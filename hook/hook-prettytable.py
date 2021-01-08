de19b1d7cb2263ad89ee387e9238f8dbd0d3022b
郝明 <haominga@dcits.com>
2021-01-08 12:56:01 +0800

    中文乱码

--- hook/hook-prettytable.py
+++ hook/hook-prettytable.py
@@ -1,2 +1,3 @@
-from PyInstaller.utils.hooks import collect_data_files
-datas = collect_data_files('prettytable')
+from PyInstaller.utils.hooks import copy_metadata
+
+datas = copy_metadata('prettytable')
